db936b36ffffe16fd8de340f385a1c32db3770db
asadchev <asadchev@gmail.com>
2020-04-27 20:54:34 -0400

    TA finalize at exit

--- python/src/tiledarray.cc
+++ python/src/tiledarray.cc
@@ -1,5 +1,15 @@
 #include "TiledArray/python/module.h"
 
 PYBIND11_MODULE(tiledarray, m) {
+
   TiledArray::python::initialize(m);
+
+  auto atexit = py::module::import("atexit");
+  atexit.attr("register")(
+    py::cpp_function([]() {
+        TiledArray::python::finalize();
+      }
+    )
+  );
+
 }
